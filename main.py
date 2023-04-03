4f07eafc90090e198b6e480b17dc4756ce07748a
greyinghair <58940142+greyinghair@users.noreply.github.com>
2023-04-03 17:20:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import boto3
 import csv
 import logging
+import json
 
-def traverse_ou(org_units, account_ou_map):
+def traverse_ou(org_units, account_ou_map, current_path=""):
     for ou in org_units:
+        ou_path = current_path + "/" + ou['Name'] if current_path else ou['Name']
         accounts = client.list_accounts_for_parent(ParentId=ou['Id'])['Accounts']
         for account in accounts:
-            account_ou_map[account['Id']] = ou['Name']
+            account_ou_map[account['Id']] = ou_path
         
         children = client.list_organizational_units_for_parent(ParentId=ou['Id'])['OrganizationalUnits']
-        traverse_ou(children, account_ou_map)
+        traverse_ou(children, account_ou_map, ou_path)
 
 # Configure logging
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Replace this with your list of account numbers
-account_numbers = ['123456789012', '234567890123', '345678901234']
+account_numbers = ['123456789012', '234567890123', '345678901234', '123456789012']
+
+# Remove duplicate account numbers
+account_numbers = list(set(account_numbers))
 
 # Initialize AWS Organizations client
 client = boto3.client('organizations')
@@ -31,45 +36,55 @@ org_units = client.list_organizational_units_for_parent(ParentId=root['Id'])['Or
 account_ou_map = {}
 traverse_ou(org_units, account_ou_map)
 
-# Get account details
-for account_number in account_numbers:
-    try:
-        account = client.describe_account(AccountId=account_number)['Account']
-    except client.exceptions.AccountNotFoundException:
-        logging.error(f"Account not found: {account_number}")
-        continue
+def main():
+    # Get account details
+    for account_number in account_numbers:
+        try:
+            account = client.describe_account(AccountId=account_number)['Account']
+        except client.exceptions.AccountNotFoundException:
+            logging.error(f"Account not found: {account_number}")
+            continue
 
-    # Get OU of the account
-    ou_name = account_ou_map.get(account_number)
+        # Get OU of the account
+        ou_path = account_ou_map.get(account_number)
+        if ou_path is None:
+            logging.warning(f"OU not found for account: {account_number}")
 
-    # Get account tags
-    try:
-        tags = client.list_tags_for_resource(ResourceId=account['Id'])['Tags']
-    except Exception as e:
-        logging.error(f"Error fetching tags for account {account_number}: {e}")
-        tags = []
+        # Get account tags
+        try:
+            tags = client.list_tags_for_resource(ResourceId=account['Id'])['Tags']
+        except Exception as e:
+            logging.error(f"Error fetching tags for account {account_number}: {e}")
+            tags = []
 
-    tags_dict = {tag['Key']: tag['Value'] for tag in tags}
-    owner_name = tags_dict.get('OwnerName')
-    route_domain = tags_dict.get('route-domain')
-    environment = tags_dict.get('Environment')
+        tags_dict = {tag['Key']: tag['Value'] for tag in tags}
+        owner_name = tags_dict.get('OwnerName')
+        route_domain = tags_dict.get('route-domain')
+        environment = tags_dict.get('Environment')
 
-    # Append account info to output list
-    output.append({
-        'AccountNumber': account_number,
-        'AccountName': account['Name'],
-        'OU': ou_name,
-        'OwnerName': owner_name,
-        'RouteDomain': route_domain,
-        'Environment': environment
-    })
+        # Append account info to output list
+        output.append({
+            'AccountNumber': int(account_number),
+            'AccountName': account['Name'],
+            'OUPath': ou_path,
+            'OwnerName': owner_name,
+            'RouteDomain': route_domain,
+            'Environment': environment
+        })
 
-# Print output and write to CSV
-print(output)
+    # Pretty-print output to screen
+    print(json.dumps(output, indent=2))
 
-with open('aws_accounts.csv', 'w', newline='') as csvfile:
-    fieldnames = ['AccountNumber', 'AccountName', 'OU', 'OwnerName', 'RouteDomain', 'Environment']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for row in output:
-        writer.writerow(row)
+def csvoutput():
+    # Write output to CSV
+    with open('aws_accounts.csv', 'w', newline='') as csvfile:
+        fieldnames = ['AccountNumber', 'AccountName', 'OUPath', 'OwnerName', 'RouteDomain', 'Environment']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in output:
+            writer.writerow(row)
+
+        
+if __name__ == '__main__':
+    main()
+    csvoutput()
