8116704311136253e69a7b4bcc4f0fb81487758f
greyinghair <58940142+greyinghair@users.noreply.github.com>
2023-04-03 14:27:05 +0100

    add id name in main

--- awsorg.py
+++ awsorg.py
@@ -18,46 +18,47 @@ output = []
 root = client.list_roots()['Roots'][0]
 org_units = client.list_organizational_units_for_parent(ParentId=root['Id'])['OrganizationalUnits']
 
-# Get account details
-for account_number in account_numbers:
-    try:
-        account = client.describe_account(AccountId=account_number)['Account']
-    except client.exceptions.AccountNotFoundException:
-        logging.error(f"Account not found: {account_number}")
-        continue
+def main():
+    # Get account details
+    for account_number in account_numbers:
+        try:
+            account = client.describe_account(AccountId=account_number)['Account']
+        except client.exceptions.AccountNotFoundException:
+            logging.error(f"Account not found: {account_number}")
+            continue
 
-    # Get OU of the account
-    ou_name = None
-    for ou in org_units:
-        accounts = client.list_accounts_for_parent(ParentId=ou['Id'])['Accounts']
-        if any(a['Id'] == account_number for a in accounts):
-            ou_name = ou['Name']
-            break
+        # Get OU of the account
+        ou_name = None
+        for ou in org_units:
+            accounts = client.list_accounts_for_parent(ParentId=ou['Id'])['Accounts']
+            if any(a['Id'] == account_number for a in accounts):
+                ou_name = ou['Name']
+                break
 
-    # Get account tags
-    try:
-        tags = client.list_tags_for_resource(ResourceId=account['Id'])['Tags']
-    except Exception as e:
-        logging.error(f"Error fetching tags for account {account_number}: {e}")
-        tags = []
+        # Get account tags
+        try:
+            tags = client.list_tags_for_resource(ResourceId=account['Id'])['Tags']
+        except Exception as e:
+            logging.error(f"Error fetching tags for account {account_number}: {e}")
+            tags = []
 
-    tags_dict = {tag['Key']: tag['Value'] for tag in tags}
-    owner_name = tags_dict.get('OwnerName')
-    route_domain = tags_dict.get('route-domain')
-    environment = tags_dict.get('Environment')
+        tags_dict = {tag['Key']: tag['Value'] for tag in tags}
+        owner_name = tags_dict.get('OwnerName')
+        route_domain = tags_dict.get('route-domain')
+        environment = tags_dict.get('Environment')
 
-    # Append account info to output list
-    output.append({
-        'AccountNumber': account_number,
-        'AccountName': account['Name'],
-        'OU': ou_name,
-        'OwnerName': owner_name,
-        'RouteDomain': route_domain,
-        'Environment': environment
-    })
+        # Append account info to output list
+        output.append({
+            'AccountNumber': account_number,
+            'AccountName': account['Name'],
+            'OU': ou_name,
+            'OwnerName': owner_name,
+            'RouteDomain': route_domain,
+            'Environment': environment
+        })
 
-# Print output and write to CSV
-print(output)
+    # Print output and write to CSV
+    print(output)
 
 with open('aws_accounts.csv', 'w', newline='') as csvfile:
     fieldnames = ['AccountNumber', 'AccountName', 'OU', 'OwnerName', 'RouteDomain', 'Environment']
@@ -65,3 +66,6 @@ with open('aws_accounts.csv', 'w', newline='') as csvfile:
     writer.writeheader()
     for row in output:
         writer.writerow(row)
+        
+ if __name__ == '__main__':
+    main()
